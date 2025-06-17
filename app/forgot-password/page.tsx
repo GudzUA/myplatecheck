0533f6a371eaeb9c33bb27d31e34d534815106f1
Yaroslav <gudz80@gmail.com>
2025-06-17 12:20:15 -0500

    fix: add uuid dependency

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useLanguage } from "../../context/LanguageContext";
 import { translations } from "../../translations";
