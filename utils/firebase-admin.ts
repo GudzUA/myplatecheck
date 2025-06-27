93d745b1de4190339fd13e11e25c94962818aeac
Yaroslav <gudz80@gmail.com>
2025-06-27 12:06:26 -0500

    fix: add firebase bucket and service account via env

--- utils/firebase-admin.ts
+++ utils/firebase-admin.ts
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
     storageBucket: "myplatecheck-8b211.appspot.com",
   });
 }
@@ -12,3 +14,4 @@ if (!admin.apps.length) {
 const bucket = admin.storage().bucket();
 
 export { admin, bucket };
+
