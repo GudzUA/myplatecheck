bf4e5a3eac31fdbb554b7f2ec7bb5c9ec523afe1
Yaroslav <gudz80@gmail.com>
2025-06-27 13:14:58 -0500

    fix: switch to env-based firebase admin config

--- utils/firebase-admin.ts
+++ utils/firebase-admin.ts
@@ -1,17 +1,18 @@
 import admin from "firebase-admin";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: privateKey,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
-    storageBucket: "myplatecheck-8b211.appspot.com",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
 
 const bucket = admin.storage().bucket();
 
 export { admin, bucket };
-
